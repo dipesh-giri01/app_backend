94ced5d0625d856ce3ce4744f4e1a1abffcc737d
Dipesh Giri <giridipesh01@gmail.com>
2025-11-27 17:11:42 +0545

    import check

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import test from "./Test/test.js";
 
 const app = express()
 console.log("Hello TypeScript + Node.js!");
 
 const PORT = 3000
-
+test()
 app.get("/", (req, res) => {
     res.json({ message: "Hello from TypeScript Express!" })
 })
